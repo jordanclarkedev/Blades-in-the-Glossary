0db3a6bd0bf0f85d06814f3504cb6bfa783e77df
jordanclarkedev <62496066+jordanclarkedev@users.noreply.github.com>
2020-04-28 16:41:47 +1000

    Fixing typo

--- embeds/engagement.js
+++ embeds/engagement.js
@@ -5,13 +5,15 @@ module.exports.engagement = `**Engagement Roll**
 - Operation is particularly bold or daring.
 - Plan’s detail exposes a vulnerability of the target / hits them where they’re weakest.
 - Friends/contacts provide aid or insight.
-- Target is a lower tier.oDistrict modifiers.
+- Target is a lower tier.
+- District modifiers.
 
 **Major Disadvantage** (-1d for each):
 - Operation is overly complex / contingent on many factors.
 - Target is strong against this approach / have particular defenses or preparations.
 - Enemies/rivals interfering in the operation.
-- Target is a higher tier.oDistrict modifiers.
+- Target is a higher tier.
+- District modifiers.
 
 **Result**
 1-3: Start in a Desperate position.
