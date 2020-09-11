b12385394c8eeed459eea69ea99a5c379007b93e
jordanclarkedev <62496066+jordanclarkedev@users.noreply.github.com>
2020-09-11 19:29:46 +1000

    Correcting error on heritage.js

--- embeds/references/heritage.js
+++ embeds/references/heritage.js
@@ -16,17 +16,17 @@ Akoros is the largest and most industrialized land in the Imperium, and is home 
 module.exports.daggerisles = `**Dagger Isles**
 If you want to be a rootless wanderer you could be from the Dagger Isles. People there often grow up on ships and travel a lot before settling down. They’re known as corsairs and merchants who live without lightning barriers— dealing with spirits in other ways.
 
-A land of windswept plains, covered in dark scrub and thorny growths. Outside the Imperial cities on the coast, some native Serverosi still live in free tribes, scavenging the death-lands on their ghost-hunting horses. They are generally dark-skinned and dark haired.`;
+A tropical archipelago covered in dense jungle growth; now turned dark and twisted from the strange magic of the cataclysm. Some say that the people there live without lightning barriers. How do they manage that? Native islanders are generally copper-skinned and dark-haired.`;
 
 module.exports.iruvia = `**Iruvia**
 If you want to be from a culture considered “foreign” by the locals, you could be from Iruvia, a rich and powerful desert kingdom far to the south. It’s another diverse land of varying cultures similar to old Persia, Egypt, and India.
 
 A land of black deserts obsidian mountains, and raging volcanoes. Some say that positions of power are openly held by demons in Iruvia. The people are generally dark-skinned and dark haired.`;
 
-module.exports.severos = `**Severos
+module.exports.severos = `**Severos**
 If you want to be from a place considered “wild” by the rest of the empire, you could be from Severos. Outside the few Imperial settlements, most Severosi live in nomadic horse-tribes scattered across the blasted deathlands, surviving within the ruins of ancient arcane fortresses which still repel spirits.
 
-A tropical archipelago covered in dense jungle growth; now turned dark and twisted from the strange magic of the cataclysm. Some say that the people there live without lightning barriers. How do they manage that? Native islanders are generally copper-skinned and dark haired.`;
+A land of windswept plains, covered in dark scrub and thorny growths. Outside the Imperial cities on the coast, some native Serverosi still live in free tribes, scavenging the death-lands on their ghost-hunting horses. They are generally brown-skinned and dark-haired.`;
 
 module.exports.skovlan = `**Skovlan**
 If you want to be from a marginalized people, you could be from Skovlan, the island kingdom just across the sea from Doskvol. Skovlan was last to be brought under Imperial rule, over the course of the 36-year Unity War (which ended only a few years ago). Many Skovlander refugees who lost their homes and jobs in the destruction of the war have come to Doskvol seeking new opportunities.
@@ -36,4 +36,4 @@ A ragged land of cold mountains and rough tundra. Skovlan was the last holdout a
 module.exports.tycheros = `**Tycheros**
 If you want to be weird, you can be from Tycheros. It’s a semi-mythical place, far away beyond the northern Void Sea. Everyone says that the people there are part-demon. If you choose Tycherosi heritage, also create a demonic telltale (like black shark eyes, feathers instead of hair, etc.) that marks your character.
 
-far off land, disconnected from the Empire. People say the Tycherosi (rudely called “Strangers”) have demon blood in their lineage.`;
+A far off land, disconnected from the Empire. People say the Tycherosi (rudely called “Strangers”) have demon blood in their lineage.`;
